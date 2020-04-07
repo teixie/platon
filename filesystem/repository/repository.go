6edb670ad7286adce971617b4e8f5f4a2e280421
zusong <419894552@qq.com>
2020-04-07 17:59:23 +0800

    update:修改文件

--- filesystem/repository/repository.go
+++ filesystem/repository/repository.go
@@ -11,45 +11,45 @@ import (
 	"github.com/teixie/platon/helpers"
 )
 
-func SaveAs(file *multipart.FileHeader, lnk string, repos string) error {
+func SaveAs(file *multipart.FileHeader, lnk string, repos string) (string, error) {
 	if _, err := os.Stat(lnk); err == nil || !os.IsNotExist(err) {
-		return errors.New("file exists")
+		return "", errors.New("file exists")
 	}
 
 	lnkPath := filepath.Dir(lnk)
 	if err := os.MkdirAll(lnkPath, os.ModePerm); err != nil {
-		return err
+		return "", err
 	}
 
 	src, err := file.Open()
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer src.Close()
 
 	body, err := ioutil.ReadAll(src)
 	if err != nil {
-		return err
+		return "", err
 	}
 	md5 := helpers.MD5(string(body))
 
 	dstPath := strings.TrimRight(repos, "/") + "/" + md5[0:2]
 	if err := os.MkdirAll(dstPath, os.ModePerm); err != nil {
-		return err
+		return "", err
 	}
 	dst := dstPath + "/" + md5
 
 	if _, err := os.Stat(dst); err != nil && os.IsNotExist(err) {
 		out, err := os.Create(dst)
 		if err != nil {
-			return err
+			return "", err
 		}
 		defer out.Close()
 
 		if _, err = out.Write(body); err != nil {
-			return err
+			return "", err
 		}
 	}
 
-	return os.Symlink(dst, lnk)
+	return dst, os.Symlink(dst, lnk)
 }
